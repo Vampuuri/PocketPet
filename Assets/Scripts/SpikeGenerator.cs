6889b9006da41e5e4babc368c9c1390182590f38
Myrsky <bakahamburger@gmail.com>
2022-01-03 01:17:20 +0200

    minigamerunner 03/01/2022 1:17

---
+++ Assets/Scripts/SpikeGenerator.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class SpikeGenerator : MonoBehaviour
+{
+    public GameObject spike;
+
+    public float MinSpeed;
+    public float MaxSpeed;
+    public float currentSpeed;
+
+    public float SpeedMultiplier;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        currentSpeed = MinSpeed;
+        generateSpike();
+    }
+
+    public void generateSpike()
+    {
+        GameObject SpikeIns = Instantiate(spike, transform.position, transform.rotation);
+
+        SpikeIns.GetComponent<SpikeScript>().spikeGenerator = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentSpeed < MaxSpeed)
+        {
+            currentSpeed += SpeedMultiplier;
+        }
+    }
+}
