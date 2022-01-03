7ffc62f3fd2cd47d6a02a268e4afd564708b7eb3
Myrsky <bakahamburger@gmail.com>
2022-01-03 19:17:02 +0200

    restart wont work

---
+++ Assets/Scripts/ControlCenter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlCenter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //proof that our scene has restarted each time we click the button
+        print("this scene has been loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetScene()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
