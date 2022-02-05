c97c647a3543ca30b82aab02ef8e39af215c3a4d
Myrsky <bakahamburger@gmail.com>
2022-02-05 20:53:03 +0200

    hunger hygiene sleep separate numbers

--- Assets/PetAttributes.cs
+++ Assets/PetAttributes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class PetAttributes : MonoBehaviour
 {
@@ -15,7 +16,13 @@ public class PetAttributes : MonoBehaviour
     public float minHunger, minHygiene, minEnergy;
     public static float hunger, hygiene, energy;
 
+    public float HungerSpeed;
+    public float HygieneSpeed;
+    public float EnergySpeed;
+
     public Text HungerTxt;
+    public Text HygieneTxt;
+    public Text EnergyTxt;
 
     private void Awake()
     {
@@ -40,16 +47,20 @@ public class PetAttributes : MonoBehaviour
         hunger = 100;
         hygiene = 100;
         energy = 100;
+        HungerSpeed = 2f;
+        HygieneSpeed = 1f;
+        EnergySpeed = 0.5f;
     }
 
     void Update()
     {
+        //nälkä
         SceneManager.sceneLoaded += OnSceneLoaded;
         if (HungerTxt != null) 
             HungerTxt.text = "HUNGER : " + hunger.ToString("F0");
         if (hunger > minHunger)
         {
-            hunger -= 1 * Time.deltaTime;
+            hunger -= HungerSpeed * Time.deltaTime;
             if(hunger <= minHunger)
             {
                 //mitä tapahtuu kun nälkä nolla?
@@ -57,14 +68,54 @@ public class PetAttributes : MonoBehaviour
             }
         }
 
+        //hygienia
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        if (HygieneTxt != null)
+            HygieneTxt.text = "HYGIENE : " + hygiene.ToString("F0");
+        if (hygiene > minHygiene)
+        {
+            hygiene -= HygieneSpeed * Time.deltaTime;
+            if (hygiene <= minHygiene)
+            {
+                //mitä tapahtuu kun hygienia nolla?
+                Die();
+            }
+        }
+
+        //energia
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        if (EnergyTxt != null)
+            EnergyTxt.text = "ENERGY : " + energy.ToString("F0");
+        if (energy > minEnergy)
+        {
+            energy -= EnergySpeed * Time.deltaTime;
+            if (energy <= minEnergy)
+            {
+                //mitä tapahtuu kun hygienia nolla?
+                Die();
+            }
+        }
     }
     
     void OnSceneLoaded (Scene gamescreen, LoadSceneMode mode)
     {
+        //nälkä
         GameObject hungerTextObject = GameObject.Find("Hunger");
         if (hungerTextObject != null)
         {
             HungerTxt = hungerTextObject.GetComponent<Text>();
+        }
+        //hygienia
+        GameObject hygieneTextObject = GameObject.Find("Hygiene");
+        if (hygieneTextObject != null)
+        {
+            HygieneTxt = hygieneTextObject.GetComponent<Text>();
+        }
+        //energia
+        GameObject energyTextObject = GameObject.Find("Energy");
+        if (energyTextObject != null)
+        {
+            EnergyTxt = energyTextObject.GetComponent<Text>();
         }
     }
 
