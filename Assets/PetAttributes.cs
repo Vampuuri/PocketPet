6bbe1d828aa3ef07e1a2f6a7b9f6fa4540897ce6
Myrsky <bakahamburger@gmail.com>
2022-02-05 16:46:14 +0200

    NÄLKÄ LASKEE KOKOAJAN OMG

--- Assets/PetAttributes.cs
+++ Assets/PetAttributes.cs
@@ -2,15 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PetAttributes : MonoBehaviour
 {
+
+    public static PetAttributes instance;
+    public static GameObject attributes;
+    public static List<GameObject> attributesList = new List<GameObject>();
 
     //Attributes
     public float minHunger, minHygiene, minEnergy;
     public static float hunger, hygiene, energy;
 
     public Text HungerTxt;
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+        attributes = GameObject.Find("EssinManager");
+
+
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            attributesList.AddRange(GameObject.FindGameObjectsWithTag("attributes"));
+            DontDestroyOnLoad(attributes);
+        }
+    }
 
     private void Start()
     {
@@ -21,7 +44,9 @@ public class PetAttributes : MonoBehaviour
 
     void Update()
     {
-        HungerTxt.text = "HUNGER : " + hunger.ToString("F0");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        if (HungerTxt != null) 
+            HungerTxt.text = "HUNGER : " + hunger.ToString("F0");
         if (hunger > minHunger)
         {
             hunger -= 1 * Time.deltaTime;
@@ -34,6 +59,15 @@ public class PetAttributes : MonoBehaviour
 
     }
     
+    void OnSceneLoaded (Scene gamescreen, LoadSceneMode mode)
+    {
+        GameObject hungerTextObject = GameObject.Find("Hunger");
+        if (hungerTextObject != null)
+        {
+            HungerTxt = hungerTextObject.GetComponent<Text>();
+        }
+    }
+
     void Die()
     {
         print("you died of hunger");
