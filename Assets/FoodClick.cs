db733b3454e9aeaad7d1af87d457a1fa6e7a4e2d
Myrsky <bakahamburger@gmail.com>
2022-02-07 01:39:51 +0200

    NÄLKÄNAPPI TOIMII

--- Assets/FoodClick.cs
+++ Assets/FoodClick.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodClick : MonoBehaviour
+
 {
+    public GameObject PetHungerObject;
+
+    private PetAttributes PetHunger;
+
+
+    void HungerSource()
+    {
+        PetHungerObject = GameObject.Find("EssinManager");
+
+    }
+
+    void Awake()
+    {
+        PetHunger = GetComponent<PetAttributes>();
+    }
+
+    private void Start()
+    {
+        Debug.Log("The hunger is " + PetAttributes.hunger);
+    }
+
     private void OnMouseDown()
     {
         Debug.Log("Clicked Food! Yum!");
+        PetAttributes.hunger += 20;
     }
 }
 
