7202b7ae201683cf1bb5f608cc6b20bd3494bf8a
Myrsky <bakahamburger@gmail.com>
2022-03-10 19:02:15 +0200

    timer progress wip

---
+++ Assets/FunctionTimer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FunctionTimer
+{
+    public static FunctionTimer Create(Action action, float timer)
+    {
+        FunctionTimer functionTimer = new FunctionTimer(action, timer);
+
+        GameObject gameObject = new GameObject("FunctionTimer", typeof(MonoBehaviourHook));
+        gameObject.GetComponent<MonoBehaviourHook>().onUpdate = functionTimer.Update;
+
+        return functionTimer;
+    }
+
+    //tämä on tässä to abuse monobehavior functions
+    public class MonoBehaviourHook : MonoBehaviour
+    {
+        public Action onUpdate;
+        private void Update()
+        {
+            if (onUpdate != null) onUpdate();
+        }
+    }
+
+    private Action action;
+    private float timer;
+    private bool isDestroyed;
+
+    private FunctionTimer(Action action, float timer)
+    {
+        this.action = action;
+        this.timer = timer;
+        isDestroyed = false;
+    }
+
+    public void Update()
+    { if (isDestroyed == false)
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                //Trigger the action, tässä tapauksessa siirretään nappi näkyviin probably
+                action();
+                DestroySelf();
+            }
+        }
+    }
+    private void DestroySelf()
+    {
+        isDestroyed = true;
+    }
+}
---
+++ Assets/Prefabs/MoveEvolveButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEvolveButton : MonoBehaviour
+{
+
+    private FunctionTimer functionTimer;
+
+    private void Start()
+    {
+        FunctionTimer.Create(MoveButton, 3f);
+    }
+
+    private void MoveButton()
+    {
+        Debug.Log("successful button move");
+    }
+}
