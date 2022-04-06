ddc11d9dacb7a7643fb9f79ea0819a94e8aa0b26
Myrsky <bakahamburger@gmail.com>
2022-04-06 14:41:14 +0300

    sprite pysyy oikeana evoluution jälkeen & ei näy väärillä ruuduilla

---
+++ Assets/PetLoad.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PetLoad : MonoBehaviour
+{
+    public static PetLoad instance;
+    public GameObject pet;
+    public Renderer rend;
+    public Scene scene;
+
+    private void Awake()
+    {
+
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+
+            DontDestroyOnLoad(pet);
+        }
+    }
+
+    private void Update()
+    {
+        rend = GetComponent<Renderer>();
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "gamescreen")
+        {
+            rend.enabled = true;
+        }
+        else
+        {
+            rend.enabled = false;
+        }
+    }
+}
