164c48f4a80db4e99b23eebc142eb429c1b18abe
Myrsky <bakahamburger@gmail.com>
2022-05-27 02:04:42 +0300

    stuck on dominant mouse controls

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -1,19 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CursorController : MonoBehaviour
 {
+    public Texture2D normalcursor;
     public Texture2D cursor;
     public Texture2D cursorClicked;
     private CursorControls controls;
 
+    public GameObject CursorControllerObject;
+
+    public static CursorController instance;
+
 
     private void Awake()
     {
-        controls = new CursorControls();
-        ChangeCursor(cursor);
+        CursorControllerObject = GameObject.Find("CursorController");
+
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(CursorControllerObject);
+        }
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "washroom")
+        {
+            //jos tämän rivin ottaa pois niin kursori vaihtuu halutusti, mutta obviously klikkaaminen ei toimi.
+            controls = new CursorControls();
+
+            ChangeCursor(cursor);
+            controls.Mouse.Click.started += _ => StartedClick();
+            controls.Mouse.Click.performed += _ => EndedClick();
+            controls.Enable();
+        }
+        else
+        {
+            //en pysty disableemaan niitä kontrolleja et se hiiri vaihtuis ja silti washroom skenessä vois klikata.
+            //ne kontrollit jostain syystä pakottaa sen sienikuvan siihen.
+            ChangeCursorNormal(normalcursor);
+            controls.Disable();
+        }
     }
+
+
 
     private void OnEnable()
     {
@@ -25,11 +64,6 @@ public class CursorController : MonoBehaviour
         controls.Disable();
     }
 
-    private void Start()
-    {
-        controls.Mouse.Click.started += _ => StartedClick();
-        controls.Mouse.Click.performed += _ => EndedClick();
-    }
 
     private void StartedClick()
     {
@@ -38,13 +72,17 @@ public class CursorController : MonoBehaviour
 
     private void EndedClick()
     {
-        ChangeCursor(cursor);
+            ChangeCursor(cursor);
     }
 
     private void ChangeCursor(Texture2D cursorType)
     {
         Vector2 hotspot = new Vector2(cursorType.width / 2, cursorType.height / 2);
-        Cursor.SetCursor(cursorType, hotspot, CursorMode.Auto);
+        Cursor.SetCursor(cursorType, hotspot, CursorMode.ForceSoftware);
     }
 
+    private void ChangeCursorNormal(Texture2D cursorType)
+    {
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
+    }
 }
