24af497750fb2ba2a83a13ee3c4128d01f4fba5f
Myrsky <bakahamburger@gmail.com>
2022-05-29 13:17:52 +0300

    evolution fix 2 siis miks se hajoo kokoaja what??

--- Assets/Scripts/MoveEvolveButton.cs
+++ Assets/Scripts/MoveEvolveButton.cs
@@ -16,12 +16,12 @@ public class MoveEvolveButton : MonoBehaviour
 
     private void MoveButton()
     {
-        if (PlayerPrefs.GetInt("EvolveBool") == 0)
+        if (PlayerPrefs.GetInt("EvolveBool") == 1)
         {
             Debug.Log("successful button move");
             GameObject evolveButton = GameObject.Find("EvolveButton");
             evolveButton.transform.Translate(Vector2.down * 7f);
-            PlayerPrefs.SetInt("EvolveBool", 1);
+            PlayerPrefs.SetInt("EvolveBool", 0);
         }
     }
 
