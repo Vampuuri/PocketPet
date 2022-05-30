0c75af0ba755e0d0c958279292a9b8159df58b3e
Myrsky <bakahamburger@gmail.com>
2022-05-30 11:07:20 +0300

    desperate attempts to fix evolution

--- Assets/Scripts/MoveEvolveButton.cs
+++ Assets/Scripts/MoveEvolveButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MoveEvolveButton : MonoBehaviour
 {
@@ -9,19 +10,34 @@ public class MoveEvolveButton : MonoBehaviour
     private FunctionTimer functionTimer;
     public GameObject myButton;
 
+
     private void Start()
     {
+        Scene currentScene = SceneManager.GetActiveScene();
         FunctionTimer.Create(MoveButton, 3f);
     }
 
     private void MoveButton()
     {
-        if (PlayerPrefs.GetInt("EvolveBool") == 1)
+        Scene currentScene = SceneManager.GetActiveScene();
+        bool inGame = currentScene.name == "gamescreen";
+
+        PlayerPrefs.SetInt("EvolveBool", 1);
+        if (inGame)
         {
-            Debug.Log("successful button move");
-            GameObject evolveButton = GameObject.Find("EvolveButton");
-            evolveButton.transform.Translate(Vector2.down * 7f);
-            PlayerPrefs.SetInt("EvolveBool", 0);
+            bool inGameAgain = inGame == true;
+            Debug.Log(PlayerPrefs.GetInt("EvolveBool"));
+            if (inGameAgain == false)
+            {
+                if (PlayerPrefs.GetInt("EvolveBool") == 1)
+                {
+                    Debug.Log("successful button move");
+                    GameObject evolveButton = GameObject.Find("EvolveButton");
+                    evolveButton.transform.Translate(Vector2.down * 7f);
+                    PlayerPrefs.SetInt("EvolveBool", 0);
+                    inGameAgain = true;
+                }
+            }
         }
     }
 
